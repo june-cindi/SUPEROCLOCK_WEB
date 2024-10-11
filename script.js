f40a4e8639cca2d8857717be57d5ac473a9ab73f
Kim Jueun <149448717+june-cindi@users.noreply.github.com>
2024-10-11 17:33:19 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.tag-button').forEach(button => {
-    button.addEventListener('click', function() {
-        document.querySelectorAll('.tag-button').forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-        document.getElementById('timer-content').style.display = 'none';
-        document.getElementById('hashtag-content').style.display = 'none';
-        document.getElementById(`${this.dataset.target}-content`).style.display = 'block';
-    });
-});
